d6a313dfc8bf03f78bb4d31a7b338da2b305b5ba
未央 <hiveforwork@163.com>
2019-10-25 21:58:11 +0800

    添加并配置了springMVC.xml,新增了IndexController,引导首页访问

---
+++ src/main/java/net/industryhive/controller/IndexController.java
@@ -0,0 +1,18 @@
+package net.industryhive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 未央
+ * @create 2019-10-25 21:28
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "index";
+    }
+
+}
