a3c2ad6d90fa7978a66392884478b711e8c0f43d
李嵩 <lisong@pansoft.com>
2019-10-29 16:09:13 +0800

    完善applicationContext.xml配置，工程框架搭建完成，接下来就是写代码了。。。

--- src/main/java/net/industryhive/test/MBGTest.java
+++ src/main/java/net/industryhive/test/MBGTest.java
@@ -1,14 +1,13 @@
 package net.industryhive.test;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
-import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MBGTest {
 	//逆向工程生成mapper文件
