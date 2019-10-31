1f8a5012aa11528b1d8be47eeca4a274ced740fd
李嵩 <lisong@pansoft.com>
2019-10-31 11:41:56 +0800

    这次数据库彻底搞定了！！！

--- src/main/java/net/industryhive/service/UserService.java
+++ src/main/java/net/industryhive/service/UserService.java
@@ -1,5 +1,8 @@
 package net.industryhive.service;
 
+import net.industryhive.bean.User;
+import net.industryhive.dao.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,11 +12,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-//    @Autowired
-//    private UserMapper userMapper;
-//
-//    public void addUser(User newUser) {
-//        userMapper.insertSelective(newUser);
-//    }
+    @Autowired
+    private UserMapper userMapper;
+
+    public void addUser(User newUser) {
+        userMapper.insertSelective(newUser);
+    }
 
 }
