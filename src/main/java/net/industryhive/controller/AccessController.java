1c2cc3d81ac55108fb796fe4e8856b5076fd197b
李嵩 <lisong@pansoft.com>
2019-11-04 20:42:20 +0800

    添加AccessController和AccessService

---
+++ src/main/java/net/industryhive/controller/AccessController.java
@@ -0,0 +1,12 @@
+package net.industryhive.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 访问统计Controller
+ * @author 未央
+ * @create 2019-11-04 20:34
+ */
+@Controller
+public class AccessController {
+}
---
+++ src/main/java/net/industryhive/service/AccessService.java
@@ -0,0 +1,12 @@
+package net.industryhive.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 访问统计Service
+ * @author 未央
+ * @create 2019-11-04 20:35
+ */
+@Service
+public class AccessService {
+}
